fe5f6630fd9f753861fa4ac64747d558800f181a
Arthur Nunes <nunes.arthur@academico.ifpb.edu.br>
2026-07-04 09:02:39 -0300

    feat: implementando sampler.h

--- ProjetoFinal/Core/Inc/Sampler.h
+++ ProjetoFinal/Core/Inc/Sampler.h
@@ -1,12 +1,61 @@
-/*
- * Sampler.h
- *
- *  Created on: 4 de jul. de 2026
- *      Author: admin
- */
+/******************************************************************************
+ * @file Sampler.h
+ * @brief Biblioteca responsável pela aquisição e tratamento das amostras do ADC.
+ ******************************************************************************/
 
-#ifndef INC_SAMPLER_H_
-#define INC_SAMPLER_H_
+#ifndef _SAMPLER_H_
+#define _SAMPLER_H_
+
+/*******************************************************************************
+ * INCLUDES NECESSÁRIOS
+ ******************************************************************************/
+
+#include "AssertTypes.h"
+
+/*******************************************************************************
+ * CONFIGURAÇÕES
+ ******************************************************************************/
+
+/*******************************************************************************
+ * DEFINES PÚBLICOS
+ ******************************************************************************/
+
+/*******************************************************************************
+ * TIPOS DE DADOS PÚBLICOS
+ ******************************************************************************/
+
+/*******************************************************************************
+ * PROTÓTIPOS PÚBLICOS
+ ******************************************************************************/
+
+/**
+ * @brief Inicializa o módulo de amostragem.
+ */
+void Sampler_Init(void);
+
+/**
+ * @brief Atualiza a média móvel das amostras.
+ *
+ * Esta função deve ser chamada sempre que houver
+ * uma nova conversão do ADC.
+ */
+void Sampler_Update(void);
+
+/**
+ * @brief Retorna a média das amostras.
+ *
+ * @return Valor médio do ADC.
+ */
+u16 Sampler_GetAverage(void);
+
+/**
+ * @brief Retorna a média convertida para porcentagem.
+ *
+ * @return Valor entre 0 e 100.
+ */
+u8 Sampler_GetPercentage(void);
+
+#endif /* _SAMPLER_H_ */
 
 
 
