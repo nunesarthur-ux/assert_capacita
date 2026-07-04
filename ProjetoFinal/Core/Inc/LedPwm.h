1c8e9b02fe288a123e38889f9660945f9ac8a990
Arthur Nunes <nunes.arthur@academico.ifpb.edu.br>
2026-07-04 09:01:44 -0300

    feat: implementacao ledpwm.h

--- ProjetoFinal/Core/Inc/LedPwm.h
+++ ProjetoFinal/Core/Inc/LedPwm.h
@@ -1,13 +1,49 @@
-/*
- * LedPwm.h
- *
- *  Created on: 4 de jul. de 2026
- *      Author: admin
+/******************************************************************************
+ * @file LedPwm.h
+ * @brief Biblioteca responsável pelo controle dos LEDs PWM.
+ ******************************************************************************/
+
+#ifndef _LEDPWM_H_
+#define _LEDPWM_H_
+
+/*******************************************************************************
+ * INCLUDES NECESSÁRIOS
+ ******************************************************************************/
+/*******************************************************************************
+* TIPOS DE DADOS PÚBLICOS
+******************************************************************************/
+
+typedef enum
+{
+eLED_1,
+eLED_2,
+eLED_3
+
+} led_t;
+
+/*******************************************************************************
+ * PROTÓTIPOS PÚBLICOS
+ ******************************************************************************/
+
+/**
+ * @brief Inicializa o módulo PWM.
+ */
+void LedPwm_Init(void);
+
+/**
+ * @brief Atualiza o brilho dos LEDs.
+ */
+void LedPwm_Update(void);
+
+/**
+ * @brief Habilita ou desabilita o controle dos LEDs.
  */
 
-#ifndef INC_LEDPWM_H_
-#define INC_LEDPWM_H_
+/**
+ * @brief Seleciona qual LED será controlado.
+ *
+ * @param led LED selecionado.
+ */
+void LedPwm_SetLed(led_t led);
 
-
-
-#endif /* INC_LEDPWM_H_ */
+#endif /* _LEDPWM_H_ */
