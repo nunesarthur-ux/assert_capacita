3fa37b77cbf7a332e887f5ddfc90a37d5bfb8740
Arthur Nunes <nunes.arthur@academico.ifpb.edu.br>
2026-07-04 09:03:26 -0300

    feat: implementando SerialCmd.h

--- ProjetoFinal/Core/Inc/SerialCmd.h
+++ ProjetoFinal/Core/Inc/SerialCmd.h
@@ -1,12 +1,28 @@
-/*
- * SerialCmd.h
- *
- *  Created on: 4 de jul. de 2026
- *      Author: admin
- */
+/******************************************************************************
+ * @file SerialCmd.h
+ * @brief Biblioteca responsável pelo tratamento dos comandos da USART.
+ ******************************************************************************/
 
-#ifndef INC_SERIALCMD_H_
-#define INC_SERIALCMD_H_
+#ifndef _SERIALCMD_H_
+#define _SERIALCMD_H_
+
+/*******************************************************************************
+ * INCLUDES NECESSÁRIOS
+ ******************************************************************************/
+
+/*******************************************************************************
+ * PROTÓTIPOS PÚBLICOS
+ ******************************************************************************/
+
+/**
+ * @brief Inicializa o módulo.
+ */
+void SerialCmd_Init(void);
+
+/**
+ * @brief Trata os comandos recebidos pela USART.
+ */
+void SerialCmd_Update(void);
 
 
 
