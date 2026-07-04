95e5ac947b7f70a072f7c9861c13e9af64eae493
Arthur Nunes <nunes.arthur@academico.ifpb.edu.br>
2026-07-04 09:04:31 -0300

    feat:implementando button.h

--- ProjetoFinal/Core/Inc/Button.h
+++ ProjetoFinal/Core/Inc/Button.h
@@ -1,13 +1,37 @@
-/*
- * Button.h
- *
- *  Created on: 4 de jul. de 2026
- *      Author: admin
+/******************************************************************************
+ * @file Button.h
+ * @brief Biblioteca responsável pelo controle do botão.
+ ******************************************************************************/
+
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+/*******************************************************************************
+ * INCLUDES NECESSÁRIOS
+ ******************************************************************************/
+
+#include <stdbool.h>
+
+/*******************************************************************************
+ * PROTÓTIPOS PÚBLICOS
+ ******************************************************************************/
+
+/**
+ * @brief Inicializa o módulo do botão.
  */
+void Button_Init(void);
 
-#ifndef INC_BUTTON_H_
-#define INC_BUTTON_H_
+/**
+ * @brief Atualiza o estado do botão.
+ */
+void Button_Update(void);
 
+/**
+ * @brief Retorna se o sistema está congelado.
+ *
+ * @return true quando o sistema estiver congelado.
+ */
+bool Button_IsFrozen(void);
 
 
 #endif /* INC_BUTTON_H_ */
